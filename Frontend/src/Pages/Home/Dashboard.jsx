29cb70c542b9a06576b82ccd6eb648843a028487
jimit8929 <jimit8929@gmail.com>
2025-08-22 23:30:22 +0530

    Dashboard continued

--- Frontend/src/Pages/Home/Dashboard.jsx
+++ Frontend/src/Pages/Home/Dashboard.jsx
@@ -2,10 +2,13 @@
 import {LuPlus} from "react-icons/lu";
 import {CARD_BG} from "../../utils/data.js";
 import toast from "react-hot-toast";
-
+import moment from "moment";
 import DashBoardLayout from '../../components/Layouts/DashBoardLayout.jsx';
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance.js";
+import { API_PATHS } from "../../utils/apiPaths.js";
+import SummaryCard from "../../components/Cards/SummaryCard.jsx";
 
 
 const Dashboard = () => {
@@ -18,7 +21,13 @@ const Dashboard = () => {
 
 
   const fetchAllSessions = async() => {
-
+    try{
+      const response = await axiosInstance.get(API_PATHS.SESSION.GET_ALL);
+      setSessions(response.data);
+    }
+    catch(error){
+      console.error("Error fetching session data:" , error);
+    }
   }
 
   const deleteSession = async(sessionData) => {
@@ -31,9 +40,22 @@ const Dashboard = () => {
 
   return (
     <DashBoardLayout>
-      <div className="container mx-auto pt-4 pb-4">
+      <div className="container mx-auto pt-4 pb-4 pl-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-0">
-
+          {sessions?.map((data , index) => (
+            <SummaryCard 
+  key={data?._id}
+  colors={CARD_BG[index % CARD_BG.length]}
+  role={data?.role || ""}
+  topicsToFocus={data?.topicsToFocus || ""}
+  experience={data?.experience || "-"}
+  questions={data?.questions?.length || "-"}
+  description={data?.description || ""}
+  lastUpdated={data?.updatedAt ? moment(data.updatedAt).format("Do MMMM YYYY") : ""}
+  onSelect={() => navigate(`/interview-prem/${data?._id}`)}
+  onDelete={() => setOpenDeleteAlert({ open: true, data })}
+/>
+          ))}
         </div>
 
         <button className="h-12 md:h-12 flex items-center justify-center gap-3 bg-linear-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white transition-colors cursor-pointer hover:shadow-2xl hover:shadow-orange-300 fixed bottom-10 md:bottom-20 right-10 md:right-20" onClick={() => setOpenCreateModal(true)}>
